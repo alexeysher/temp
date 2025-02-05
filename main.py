cecea3fa5d97e7f6942b80504d080bd8d181698d
ALEXEY SHERSTOBITOV <107345313+alexeysher@users.noreply.github.com>
2025-02-06 00:31:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import gdown
 
 url = "https://drive.google.com/file/d/1FK9WQ8ybhP1sohyB4sg9tBo4rT9xi3mv/view?usp=sharing"
-gdown.cached_download(url, path='./model', postprocess=gdown.extractall, fuzzy=True)
+gdown.cached_download(url, path='model', postprocess=gdown.extractall, fuzzy=True)
