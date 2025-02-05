4b75d81fcdaaab12d8c8761d28e2c3bb12e492be
ALEXEY SHERSTOBITOV <107345313+alexeysher@users.noreply.github.com>
2025-02-06 00:34:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import gdown
 
+from pathlib import Path
+
+print(Path('.').absolute())
+
 url = "https://drive.google.com/file/d/1FK9WQ8ybhP1sohyB4sg9tBo4rT9xi3mv/view?usp=sharing"
 gdown.cached_download(url, path='model', postprocess=gdown.extractall, fuzzy=True)
