2547b7220bd5c77c8a9385169ca607b384289683
ALEXEY SHERSTOBITOV <107345313+alexeysher@users.noreply.github.com>
2025-02-06 01:05:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from keras import models
 print(Path('.').absolute())
 
 url = "https://drive.google.com/file/d/1u_5ifSE9mZiuS3364mUXl-F1tQ2xGz6G/view?usp=sharing"
-path = "model.h5"
+path = "model"
 gdown.download(url, path)
 models.load_model(filepath=path, compile=False)
