3b139d8b5cd0aeeb31b66c5f377cc3ac3fcccff5
ALEXEY SHERSTOBITOV <107345313+alexeysher@users.noreply.github.com>
2025-02-06 00:52:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from pathlib import Path
 
 print(Path('.').absolute())
 
-url = "https://drive.google.com/file/d/1FK9WQ8ybhP1sohyB4sg9tBo4rT9xi3mv/view?usp=sharing"
-path = "/mount/src/temp/model.zip"
+url = "https://drive.google.com/file/d/1u_5ifSE9mZiuS3364mUXl-F1tQ2xGz6G/view?usp=sharing"
+path = "model.h5"
 gdown.download(url, path)
