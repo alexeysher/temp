8cb4c12a16ca1284d35c24e4281f55d58e7df535
ALEXEY SHERSTOBITOV <107345313+alexeysher@users.noreply.github.com>
2025-02-06 00:42:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ print(Path('.').absolute())
 
 url = "https://drive.google.com/file/d/1FK9WQ8ybhP1sohyB4sg9tBo4rT9xi3mv/view?usp=sharing"
 path = "/mount/src/temp/model.zip"
-gdown.cached_download(url, path=path, fuzzy=True)
+gdown.download(url, path=path, fuzzy=True)
