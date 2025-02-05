e3a56f8d1b02bb956f6e5a9058dfa0b2d8ad32cb
ALEXEY SHERSTOBITOV <107345313+alexeysher@users.noreply.github.com>
2025-02-06 00:35:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from pathlib import Path
 print(Path('.').absolute())
 
 url = "https://drive.google.com/file/d/1FK9WQ8ybhP1sohyB4sg9tBo4rT9xi3mv/view?usp=sharing"
-gdown.cached_download(url, path='model', postprocess=gdown.extractall, fuzzy=True)
+# gdown.cached_download(url, path='model', postprocess=gdown.extractall, fuzzy=True)
