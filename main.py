3b9c3d4b1c8fb7b2483c474f45cf42f717739d1d
ALEXEY SHERSTOBITOV <107345313+alexeysher@users.noreply.github.com>
2025-02-06 00:43:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ print(Path('.').absolute())
 
 url = "https://drive.google.com/file/d/1FK9WQ8ybhP1sohyB4sg9tBo4rT9xi3mv/view?usp=sharing"
 path = "/mount/src/temp/model.zip"
-gdown.download(url, path=path)
+gdown.download(url, path)
