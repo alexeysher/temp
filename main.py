a51491b26b44afbcd7d2b2100f1527762d51178e
ALEXEY SHERSTOBITOV <107345313+alexeysher@users.noreply.github.com>
2025-02-06 02:23:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from st_files_connection import FilesConnection
 
 conn = st.connection('gcs', type=FilesConnection)
 path = "final_model.h5"
-_ = conn.read(path, input_format='text')
+_ = conn.read(path)
 # models.load_model(filepath=path, compile=False)
